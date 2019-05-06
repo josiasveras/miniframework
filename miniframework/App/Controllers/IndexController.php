0b72a111fc395d711a15a3f2b6b8bb60006ecad4
Jorzias Veras <josias_veras@hotmail.com>
2019-05-06 16:38:11 -0300

    508. Views-14min

--- miniframework/App/Controllers/IndexController.php
+++ miniframework/App/Controllers/IndexController.php
@@ -3,14 +3,28 @@
 namespace App\Controllers;
 
 class IndexController {
-
+	
 	public function index() {
-		echo 'Chegamos ao IndexController e disparamos a action index';
+
+	//A variável $dados é passada com segundo parâmetro da função render para que seja possível recuperar seu conteúdo dentro do escopo em que ela não existe
+	   	$dados = array ('sofá', 'cadeira', 'cama');
+		$this->render('index', $dados);
+
 	}
 
 	public function sobreNos() {
-		echo 'Chegamos ao IndexController e disparamos a action sobreNos';
+
+		$dados = array ('notebook', 'smarphone', 'tablet');
+		$this->render('sobreNos', $dados);
+
 	}
-}
+
+	public function render($view, $dados) {
+		echo get_class($this);
+		require_once "../App/Views/index/".$view.".phtml";
+
+	}
+
+}	
 
 ?>
