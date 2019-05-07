ab50b5f788457b4f742cb9770d5ec12233b0c7ff
Jorzias Veras <josias_veras@hotmail.com>
2019-05-07 13:16:35 -0300

    Add  508. Views

--- miniframework/App/Controllers/IndexController.php
+++ miniframework/App/Controllers/IndexController.php
@@ -3,25 +3,36 @@
 namespace App\Controllers;
 
 class IndexController {
+
+	private $view;
+
+	public function __construct(){
+		$this->view = new \stdClass();
+	}
 	
 	public function index() {
 
 	//A variável $dados é passada com segundo parâmetro da função render para que seja possível recuperar seu conteúdo dentro do escopo em que ela não existe
-	   	$dados = array ('sofá', 'cadeira', 'cama');
-		$this->render('index', $dados);
+	   	$this->view->dados = array ('sofá', 'cadeira', 'cama');
+		$this->render('index');
 
 	}
 
 	public function sobreNos() {
 
-		$dados = array ('notebook', 'smarphone', 'tablet');
-		$this->render('sobreNos', $dados);
+		$this->view->dados = array ('notebook', 'smarphone', 'tablet');
+		$this->render('sobreNos');
 
 	}
 
-	public function render($view, $dados) {
-		echo get_class($this);
-		require_once "../App/Views/index/".$view.".phtml";
+	public function render($view) {
+		$classAtual = get_class($this);
+
+		$classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+
+		$classAtual = strtolower(str_replace('Controller', '', $classAtual));
+
+		require_once "../App/Views/".$classAtual."/".$view.".phtml";
 
 	}
 
